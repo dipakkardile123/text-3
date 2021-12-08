d3e4a9871c71a5f756a9ad2e7f918fcddda26254
dipakk kardile <dipakkardile71@gmail.com>
2021-12-08 11:17:39 +0545

    Done

---
+++ lessOrGreaterThan.js
@@ -0,0 +1,11 @@
+function dk(a,b){
+    if (a>b){
+        console.log(a,"is greter than b");
+    }else if(a<b){
+        console.log(a,"is less than b");
+    } else{
+        console.log (a,"a,b is same");
+    }        
+}
+
+dk(64,67);
